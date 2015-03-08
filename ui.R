831dd4ffbba05a020bc4a001afb8644c389058e1
Roberto Serrano <rserranon@gmail.com>
2015-03-07 18:01:47 -0600

    Initial commit

---
+++ ui.R
@@ -0,0 +1,37 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      
+      
+      HTML('
+        <label for="twitterUser">Cuenta de Twitter:</label>
+        <input id="twitterUser" type="text" class="form-control" value="@StartupsPal"/>
+      '),
+        
+      sliderInput("followers",
+                  "Numero de followers:",
+                  min = 1,
+                  max = 5000,
+                  value = 100)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
